ab5056ea24cd0b0a00993edfed0aa4d17a70bec6
ia17011 <cfoyvlird@gmail.com>
2020-09-16 14:04:55 +0900

    feat: 🎸 can fetch contribution by scraping

--- github/client.go
+++ github/client.go
@@ -1,122 +1,53 @@
 package github
 
 import (
-	"context"
-	"encoding/json"
-	"net/http"
+	"fmt"
+	"regexp"
+	"strconv"
 	"time"
 
-	"github.com/google/go-github/v32/github"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/microcosm-cc/bluemonday"
 	"github.com/pkg/errors"
 )
 
-type GitHubClient struct {
-	service *github.Client
+// htmlから正規表現を用いて今日のcontribution数を抜き出す関数
+func extractContributionCount(html string) int {
+	// bluemondayの設定
+	p := bluemonday.UGCPolicy()
+	p.AllowElements("rect")
+	p.AllowAttrs("data-count", "data-date").OnElements("rect")
+
+	t := time.Now().Format("2006-01-02")
+	sanitaizedHtml := p.Sanitize(html)
+
+	pattern := fmt.Sprintf("(?i)<rect data-count=\"(\\d+)\" data-date=\"%s\"></rect>", t)
+	re := regexp.MustCompile(pattern)
+	strContributionCount := re.FindStringSubmatch(sanitaizedHtml)[1]
+	contributionCount, _ := strconv.Atoi(strContributionCount)
+
+	return contributionCount
 }
 
-func New(c *http.Client) GitHubClient {
-	return GitHubClient{service: github.NewClient(c)}
-}
-
-func isValidEventType(arr []string, str string) bool {
-	for _, v := range arr {
-		if v == str {
-			return true
-		}
-	}
-	return false
-}
-
-func isValidEvent(event string, durationMinutes float64) bool {
-	const minutesADay = 1440.0
-	contributionCountEvents := []string{"PushEvent", "IssuesEvent", "PullRequestEvent"}
-
-	return isValidEventType(contributionCountEvents, event) && minutesADay >= durationMinutes
-}
-
-// NOTE: count commit size
-func getPushEventContribution(payload json.RawMessage) (int, error) {
-	var pushEventPayload *github.PushEvent
-	if err := json.Unmarshal([]byte(payload), &pushEventPayload); err != nil {
-		return 0, err
-	}
-	return *pushEventPayload.Size, nil
-}
-
-// NOTE: count when only opend
-func getIssuesEventContribution(payload json.RawMessage) (int, error) {
-	var issuesEventPayload *github.IssuesEvent
-	if err := json.Unmarshal([]byte(payload), &issuesEventPayload); err != nil {
-		return 0, err
-	}
-	action := issuesEventPayload.GetAction()
-	if action == "opened" {
-		return 1, nil
-	}
-	return 0, nil
-}
-
-// NOTE: count when only opend
-func getPullRequestContribution(payload json.RawMessage) (int, error) {
-	var pullRequestEventPayload *github.PullRequestEvent
-	if err := json.Unmarshal([]byte(payload), &pullRequestEventPayload); err != nil {
-		return 0, err
-	}
-	action := pullRequestEventPayload.GetAction()
-	if action == "opened" {
-		return 1, nil
-	}
-	return 0, nil
-}
-
-func getContributionCount(events []*github.Event) (int, error) {
-	count := 0
-	for _, event := range events {
-		var contributionNum int
-		var err error
-
-		payload := event.GetRawPayload()
-		eventType := event.GetType()
-		eventDay := event.GetCreatedAt()
-		durationMinutes := time.Since(eventDay).Minutes()
-
-		// log.Printf("eventDay: %v, durationMinutes: %f",eventDay, durationMinutes)
-		if isValidEvent(eventType, durationMinutes) != true {
-			continue
-		}
-
-		switch eventType {
-		case "PushEvent":
-			contributionNum, err = getPushEventContribution(payload)
-			if err != nil {
-				return 0, err
-			}
-		case "IssuesEvent":
-			contributionNum, err = getIssuesEventContribution(payload)
-			if err != nil {
-				return 0, err
-			}
-		case "PullRequestEvent":
-			contributionNum, err = getPullRequestContribution(payload)
-			if err != nil {
-				return 0, err
-			}
-		}
-		count += contributionNum
-	}
-
-	return count, nil
-}
-
-func (gc *GitHubClient) GetTodaysPublicContributions(userName string) (int, error) {
-	// TODO: add option to handle private contribution in the future
-	events, _, err := gc.service.Activity.ListEventsPerformedByUser(context.Background(), userName, true, nil)
-	if _, ok := err.(*github.RateLimitError); ok {
-		return 0, errors.Wrapf(err, "hit late limit")
-	}
-	count, err := getContributionCount(events)
+// rectタグに近いDOMを手に入れる関数
+func getTargetCloseHtml(doc *goquery.Document) (string, error) {
+	html, err := doc.Find("div.js-calendar-graph svg.js-calendar-graph-svg g").Html()
 	if err != nil {
-		return 0, errors.Wrapf(err, "event handling error")
+		return "", errors.Wrapf(err, "dom get failed")
 	}
-	return count, nil
+	return html, nil
+}
+
+func GetTodaysPublicContributions(userName string) (int, error) {
+	targetUrl := "https://github.com/" + userName
+
+	doc, err := goquery.NewDocument(targetUrl)
+	if err != nil {
+		return 0, errors.Wrapf(err, "cant fetch document")
+	}
+
+	html, _ := getTargetCloseHtml(doc)
+	contributionCount := extractContributionCount(html)
+
+	return contributionCount, nil
 }
